6c1471a98fa3218a3d32156134d1182b019c2aa7
mca-programmer <freelancermca52@gmail.com>
2025-12-30 20:15:48 +0600

    added index.js

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//  Enable CORS for all origins
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
+
+
+
+
+
+
+
+        console.log("MongoDB Connected!");
+    } finally { }
+}
 
 run().catch(console.dir);
 
